c7b6e9bef5395d00412e1afc208b973fa17f14b4
Kelvin Zhang <me@iamkelv.in>
2021-05-10 15:23:57 +0100

    Run experiments under threads so progress bar will work

--- experiments/constant_load_experiment.py
+++ experiments/constant_load_experiment.py
@@ -1,8 +1,11 @@
 import json
 import os
+import queue
 import subprocess
+import threading
 
-from progressbar import progressbar
+import time
+from progressbar import bar
 
 import api_client
 from config import Config
@@ -11,6 +14,9 @@ from helpers import generate_output_path
 
 
 class ConstantLoadExperiment(Experiment):
+
+    iterationErrorsQueue = queue.Queue(maxsize=1)
+
     def __init__(
         self,
         config: Config,
@@ -38,25 +44,43 @@ class ConstantLoadExperiment(Experiment):
 
         iteration_metrics = []
 
-        for _ in progressbar(range(self.iterations), redirect_stdout=True):
+        iterationBar = bar.ProgressBar(redirect_stdout=True)
+        for _ in iterationBar(range(self.iterations), redirect_stdout=True):
+            iterationBar.update(force=True)
+            thread = threading.Thread(
+                target=self.__run, args=(k6_env, iteration_metrics)
+            )
+            thread.daemon = True
+            thread.start()
+
+            while thread.is_alive():
+                time.sleep(0.25)
+                iterationBar.update(force=True)
+
+            if self.iterationErrorsQueue.qsize() > 0:
+                print("Stopping early due to error")
+                exit()
+
+        with open(generate_output_path(suffix="constant-load"), "w") as output_file:
+            json.dump(iteration_metrics, output_file)
+        print("Complete:")
+        print(iteration_metrics)
+
+    def __run(self, k6_env, iteration_metrics):
+        try:
             if not api_client.empty_cart(self.config).ok:
-                print(f"unable to empty cart")
-                exit()
+                raise RuntimeError(f"unable to empty cart")
             if not api_client.seed_cart(self.config, 200000).ok:
-                print(f"unable to seed cart")
-                exit()
+                raise RuntimeError(f"unable to seed cart")
             if not api_client.set_dimming_mode(self.config, self.dimming_mode).ok:
-                print(f"unable to set dimming mode")
-                exit()
+                raise RuntimeError(f"unable to set dimming mode")
 
             if self.use_component_weightings:
                 if not api_client.set_component_weightings(self.config).ok:
-                    print(f"unable to set component weightings")
-                    exit()
+                    raise RuntimeError(f"unable to set component weightings")
             else:
                 if not api_client.clear_component_weightings(self.config).ok:
-                    print(f"unable to clear component weightings")
-                    exit()
+                    raise RuntimeError(f"unable to clear component weightings")
 
             output_path = generate_output_path(suffix="k6")
 
@@ -74,23 +98,27 @@ class ConstantLoadExperiment(Experiment):
             )
 
             if k6_process.returncode != 0:
-                print(f"unable to run k6, stderr =\n\t{k6_process.stderr}")
-                exit()
+                raise RuntimeError(f"unable to run k6, stderr =\n\t{k6_process.stderr}")
 
             with open(output_path, "r") as output_file:
                 k6_metrics = json.load(output_file)
 
                 metrics = {}
                 if "attrition" in k6_metrics["metrics"]:
-                    metrics["attrition"] = k6_metrics["metrics"]["attrition"]["values"]["count"]
+                    metrics["attrition"] = k6_metrics["metrics"]["attrition"]["values"][
+                        "count"
+                    ]
                 if "items_checked_out" in k6_metrics["metrics"]:
-                    metrics["items_checked_out"] = k6_metrics["metrics"]["items_checked_out"]["values"]["count"]
+                    metrics["items_checked_out"] = k6_metrics["metrics"][
+                        "items_checked_out"
+                    ]["values"]["count"]
                 if "recommendations_checked_out" in k6_metrics["metrics"]:
-                    metrics["recommendations_checked_out"] = k6_metrics["metrics"]["recommendations_checked_out"]["values"]["count"]
+                    metrics["recommendations_checked_out"] = k6_metrics["metrics"][
+                        "recommendations_checked_out"
+                    ]["values"]["count"]
 
                 iteration_metrics.append(metrics)
-
-        with open(generate_output_path(suffix="constant-load"), "w") as output_file:
-            json.dump(iteration_metrics, output_file)
-        print("Complete:")
-        print(iteration_metrics)
+        except Exception as e:
+            print(e)
+            self.iterationErrorsQueue.put(1)
+            exit()
--- experiments/saturation_experiment.py
+++ experiments/saturation_experiment.py
@@ -1,9 +1,11 @@
 import json
 import os
 import subprocess
+import threading
+import queue
 
 import time
-from progressbar import progressbar
+from progressbar import bar
 
 import api_client
 from config import Config
@@ -12,6 +14,9 @@ from helpers import generate_output_path
 
 
 class SaturationExperiment(Experiment):
+
+    iterationErrorsQueue = queue.Queue(maxsize=1)
+
     def __init__(self, config: Config, is_dimming_enabled: bool):
         super().__init__()
         self.config = config
@@ -43,23 +48,44 @@ class SaturationExperiment(Experiment):
             max = 501
             step = 20
 
-        for max_vus in progressbar(range(min, max, step), redirect_stdout=True):
+        iterationBar = bar.ProgressBar(redirect_stdout=True)
+        for max_vus in iterationBar(range(min, max, step)):
+            iterationBar.update(force=True)
+            thread = threading.Thread(
+                target=self.__run, args=(max_vus, k6_env, vu_metrics)
+            )
+            thread.daemon = True
+            thread.start()
+
+            while thread.is_alive():
+                time.sleep(0.25)
+                iterationBar.update(force=True)
+
+            if self.iterationErrorsQueue.qsize() > 0:
+                print("Stopping early due to error")
+                exit()
+
+        with open(
+            generate_output_path(suffix="dimmer-disabled-saturation"), "w"
+        ) as output_file:
+            json.dump(vu_metrics, output_file)
+
+        print("Complete")
+
+    def __run(self, max_vus: int, k6_env: dict, vu_metrics: dict):
+        try:
             print(f"\tStarting iteration with VUs = {max_vus}")
             if not api_client.empty_cart(self.config).ok:
-                print(f"unable to empty cart")
-                exit()
+                raise RuntimeError(f"unable to empty cart")
             if not api_client.seed_cart(self.config, 200000).ok:
-                print(f"unable to seed cart")
-                exit()
+                raise RuntimeError(f"unable to seed cart")
             if not api_client.set_dimming_mode(self.config, self.dimming_mode).ok:
-                print(f"unable to set dimming mode")
-                exit()
+                raise RuntimeError(f"unable to set dimming mode")
             if self.is_dimming_enabled:
                 # We do not want to run baseline dimming with component
                 # weightings enabled.
                 if not api_client.clear_component_weightings(self.config).ok:
-                    print(f"unable to clear component weightings")
-                    exit()
+                    raise RuntimeError(f"unable to clear component weightings")
 
             output_path = generate_output_path(suffix="k6")
 
@@ -77,23 +103,18 @@ class SaturationExperiment(Experiment):
             )
 
             if k6_process.returncode != 0:
-                print(f"unable to run k6, stderr =\n\t{k6_process.stderr}")
-                exit()
+                raise RuntimeError(f"unable to run k6, stderr =\n\t{k6_process.stderr}")
 
             # Reset the dimming mode so the response time graph does not taper.
             if not api_client.set_dimming_mode(self.config, self.dimming_mode).ok:
-                print(f"unable to set dimming mode")
-                exit()
+                raise RuntimeError(f"unable to set dimming mode")
 
             with open(output_path, "r") as output_file:
                 metrics = json.load(output_file)
                 vu_metrics[max_vus] = metrics["metrics"]["http_req_duration"]["values"]
 
             time.sleep(10)
-
-        with open(
-            generate_output_path(suffix="dimmer-disabled-saturation"), "w"
-        ) as output_file:
-            json.dump(vu_metrics, output_file)
-
-        print("Complete")
+        except Exception as e:
+            print(e)
+            self.iterationErrorsQueue.put(1)
+            exit()
